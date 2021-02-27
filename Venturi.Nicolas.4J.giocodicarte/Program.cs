777bc72fb15d181d9251d70b186f088e4a2c1b62
NikyDev <66072452+NikyDev@users.noreply.github.com>
2021-02-27 10:26:40 +0100

    primo commit

---
+++ Venturi.Nicolas.4J.giocodicarte/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venturi.Nicolas._4J.giocodicarte.models;
+
+namespace Venturi.Nicolas._4J.giocodicarte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strv1, strv2, strs1, strs2;
+            int v1, v2;
+
+            Console.WriteLine("inserisci il valore della prima carta da 2 a 14(14=asso)");
+            strv1 = Console.ReadLine();
+            v1 = Convert.ToInt32(strv1);
+            Console.WriteLine("inserisci il seme della prima carta usando la prima lettera (C=cuori, Q=quadri, F=fiori, P=picche)");
+            strs1 = Console.ReadLine();
+            carta c1 = new carta(v1, strs1);
+            Console.WriteLine(c1.visualizza(c1));
+            Console.WriteLine("inserisci il valore della seconda carta da 2 a 14(14=asso)");
+            strv2 = Console.ReadLine();
+            v2 = Convert.ToInt32(strv2);
+            Console.WriteLine("inserisci il seme della seconda carta usando la prima lettera (C=cuori, Q=quadri, F=fiori, P=picche)");
+            strs2 = Console.ReadLine();
+            carta c2 = new carta(v2, strs2);
+            if (c1.vince(c1, c2))
+            {
+                Console.WriteLine("la prima carta vince sulla seconda");
+            }
+            else
+            {
+                Console.WriteLine("la seconda carta vince sulla prima");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Venturi.Nicolas.4J.giocodicarte/models/carta.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venturi.Nicolas._4J.giocodicarte.models
+{
+    class carta
+    {
+        private int _valore;
+        private string _seme;
+
+        public int valore
+        {
+            get
+            {
+                return _valore;
+            }
+
+            set
+            {
+                _valore = value;
+            }
+        }
+
+        public string seme
+        {
+            get
+            {
+                return _seme;
+            }
+
+            set
+            {
+                _seme = value;
+            }
+        }
+
+        public carta(int v, string s)
+        {
+            valore = v;
+            seme = s.ToUpper();
+        }
+
+        public string visualizza(carta c)
+        {
+            if(c.seme=="C")
+            {
+                if(c.valore==14)
+                {
+                    return "la tua carta è l'asso di cuori";
+                }
+                else
+                {
+                    return "la tua carta è il " + c.valore + " di cuori";
+                }
+               
+            }
+            else
+            {
+                if(c.seme=="Q")
+                {
+                    if (c.valore == 14)
+                    {
+                        return "la tua carta è l'asso di quadri";
+                    }
+                    else
+                    {
+                        return "la tua carta è il " + c.valore + " di quadri";
+                    }
+                }
+                else
+                {
+                    if(c.seme=="F")
+                    {
+                        if (c.valore == 14)
+                        {
+                            return "la tua carta è l'asso di fiori";
+                        }
+                        else
+                        {
+                            return "la tua carta è il " + c.valore + " di fiori";
+                        }
+                    }
+                    else
+                    {
+                        if(c.seme=="P")
+                        {
+                            if (c.valore == 14)
+                            {
+                                return "la tua carta è l'asso di picche";
+                            }
+                            else
+                            {
+                                return "la tua carta è il " + c.valore + " di picche";
+                            }
+                        }
+                        else
+                        {
+                            return "il seme da te inserito non esiste";
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public bool vince(carta c1, carta c2)
+        {
+           
+            if(c1.seme==c2.seme)
+            {
+                if(c1.valore>c2.valore)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(c1.seme=="C")
+                {
+                    return true;
+                }
+                else
+                {
+                    if(c2.seme=="C")
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if(c1.seme=="Q")
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            if(c2.seme=="Q")
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                if(c1.seme=="F")
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    if(c2.seme=="F")
+                                    {
+                                        return false;
+                                    }
+                                    {
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
